b48d9680a850fe542f2fba151f5e733396c9e88c
tobe <t.17aym@gmail.com>
2023-05-21 22:04:52 +0900

    lineChart 実装

---
+++ src/components/LineChart.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import type { ChartData, ChartOptions } from 'chart.js'
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  RadialLinearScale,
+  Filler,
+  LineElement
+} from 'chart.js'
+
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(CategoryScale, LinearScale, BarElement,PointElement ,RadialLinearScale, LineElement, Filler, Title, Tooltip, Legend)
+
+const props = defineProps<{
+  data: ChartData<'line'>
+}>()
+
+const options:ChartOptions<'line'> = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+
+</script>
+
+<template>
+  <Line :data="props.data" :options="options"/>
+</template>
--- src/components/PrefList.vue
+++ src/components/PrefList.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
+import LineChart from './LineChart.vue'
 import PrefApi from '../repositories/prefApiRepository'
-
-const prefAll = ref([] as {
-  prefCode: number
-  prefName: string
-}[])
+import type { ChartData } from 'chart.js'
 
 type prefPerYearResult = {
   label: string
@@ -17,7 +14,11 @@ type prefPerYearResultData = {
   value: number
 }
 
-const checkedPrefs = ref([])
+// 都道府県一覧を作成
+const prefAll = ref([] as {
+  prefCode: number
+  prefName: string
+}[])
 
 const getAllPref = async () => {
   try {
@@ -28,21 +29,79 @@ const getAllPref = async () => {
   }
 }
 
-const getPrefPerYear = async (id:string) => {
+// 都道府県別人口数グラフを作成
+const count = ref<number>(0)
+const dataList = ref<string[]>([])
+const chartData = ref<{
+  prefName: string
+  prefPerList: number[]
+  borderColor: string
+}[]>([])
+
+const borderColorList= [
+  'rgba(255, 99, 132, 1)',
+  'rgba(54, 162, 235, 1)',
+  'rgba(255, 206, 86, 1)',
+  'rgba(75, 192, 192, 1)',
+  'rgba(153, 102, 255, 1)',
+  'rgba(255, 159, 64, 1)',
+]
+
+const getPrefPerYear = async (id:string, name:string) => {
   try {
-    const res = (await PrefApi.getPerYear({prefCode: id, cityCode: "-"})).data.result
-    const resResult:prefPerYearResult[] = [...res.data]
-    const prefPerYear = resResult.find(_ => _.label === '総人口')?.data
-    console.log(prefPerYear)
+    const checkedName = prefAll.value.find(_ => _.prefCode === Number(id))?.prefName
+
+    if(chartData.value.find(_ => _.prefName === checkedName)) {
+      chartData.value = chartData.value.filter(_ => _.prefName !== checkedName )
+    } else {
+      if(count.value > 5) {
+        count.value -= 6
+      }
+      count.value++
+      const res = (await PrefApi.getPerYear({prefCode: id, cityCode: "-"})).data.result
+      const resResult:prefPerYearResult[] = [...res.data]
+      const prefPerYear = resResult.find(_ => _.label === '総人口')?.data // 総人口データ
+
+      if(prefPerYear) {
+        dataList.value = prefPerYear.map(_ => {
+          return String(_.year)
+        })
+
+        const list:number[] = prefPerYear.map(_ => {
+          return _.value
+        })
+
+        const borderColor:string = borderColorList[count.value-1]
+
+        chartData.value.push({
+          prefName: name,
+          prefPerList: list,
+          borderColor: borderColor,
+        })
+
+      }
+    }
   } catch(error) {
     console.log(error)
   }
 }
 
-onMounted(() => {
+const data:ChartData<'line'> = computed(() => {
+  const dataset = chartData.value.map(_ => {
+    return {
+      label: _.prefName,
+      data: _.prefPerList,
+      borderColor: _.borderColor,
+    }
+  })
+  return {
+    labels: dataList.value,
+    datasets: dataset,
+  }
+})
 
+onMounted(() => {
   getAllPref()
-  // getPrefPerYear()
 })
 
 </script>
@@ -55,12 +114,14 @@ onMounted(() => {
           type="checkbox"
           :id="String(item.prefCode)"
           :value="String(item.prefCode)"
-          v-model="checkedPrefs"
-          @click="getPrefPerYear(String(item.prefCode))"
+          @change="getPrefPerYear(String(item.prefCode), item.prefName)"
         >
         <label :for="String(item.prefCode)">{{item.prefName}}</label>
       </div>
     </template>
+  </div>
+  <div>
+    <LineChart :data="data" />
   </div>
 </template>
 
